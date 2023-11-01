8d2b8a1c72a5c918d9de050002799d4f0aec7034
CB0nesSelf <harrodisabella@gmail.com>
2023-11-01 23:41:07 +0000

    work in progress

--- CBCBitFlip.py
+++ CBCBitFlip.py
@@ -1,47 +1,35 @@
-# The code is modified from
-# https://gist.github.com/lopes/168c9d74b988391e702aac5f4aa69e41
-#
+#Need to go through and flip each bit of ciphertext until flip bit setting admin = 1
+#Step 1: be able to send requests from python
+#Step 2: get bit flipping to work
+import requests 
 from base64 import b64decode
 from base64 import b64encode
 
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
+#Display reponse
+def displayResponse(cookies):
+    url = "http://mercury.picoctf.net:25992/"
+    #GET reponse to API
+    response = requests.get(url, cookies=cookies)  
+    response_text = response.text
+    #response_cookies = response.cookies
+    #print(response_cookies)
+    if "picoCTF" in response_text:
+        print(response_text)
 
-
-class AESCipher:
-    def __init__(self, key):
-        self.key = key
-
-    def encrypt(self, data):
-        iv = get_random_bytes(AES.block_size)
-        self.cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return b64encode(iv + self.cipher.encrypt(pad(data.encode('utf-8'), 
-            AES.block_size)))
-
-    def decrypt(self, data):
-        raw = b64decode(data)
-        self.cipher = AES.new(self.key, AES.MODE_CBC, raw[:AES.block_size])
-        return unpad(self.cipher.decrypt(raw[AES.block_size:]), AES.block_size)
-    
-def bitFlip( pos, bit, data):
+#function that does bitflip
+def bitFlip(pos, bit, data):
     raw = b64decode(data)
     list1 = list(raw)
-    list1[pos] = list1[pos] ^ bit
+    list1[pos] = list1[pos]^bit
     raw = bytes(list1)
     return b64encode(bytes(raw))
 
 
-if __name__ == '__main__':
 
-    key = b'Sixteen byte key'
-    msg = "Buy 1000 lots of waffles"
-    
-    print('Original Message:', msg)
-
-    ctx = AESCipher(key).encrypt(msg).decode('utf-8')
-    print('Ciphertext      :', ctx)
-
-    ctx = bitFlip(4,4,ctx)
-
-    print('Message...      :', AESCipher(key).decrypt(ctx).decode('utf-8'))
+#go through each bit 
+ciphertext = "RVJwME9QNll2YldWWm04NWNVUzdCUE80STZQd0p6NU1DNEp2N3l2enJMclRVNnpVanEyTjdiK0NZaGw1aitKa1ltWG0ybUJXc1FkbG9YUVVMZElNd0luYzRoZDB2a0dCckc2cWxxWWVObWJuZ2tjMngvQUxXNzhvUWNubWl3bEo="
+for pos in range(0, len(ciphertext)):
+    for bit in range(0, 8):
+        editCiphertext = str(bitFlip(pos, bit, ciphertext))
+        cookies = {'auth_name': editCiphertext}
+        displayResponse(cookies)
